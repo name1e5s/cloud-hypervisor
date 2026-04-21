29bb9230f536879ff03f83e11a916fa2c69592aa
Hai-Hsin <me@hai-hs.in>
2026-04-22 01:12:41 +0800

    feat(devices): add ivshmem PCI device

--- devices/src/lib.rs
+++ devices/src/lib.rs
@@ -18,11 +18,13 @@ pub mod gic;
 pub mod interrupt_controller;
 #[cfg(target_arch = "x86_64")]
 pub mod ioapic;
+pub mod ivshmem;
 pub mod legacy;
 pub mod pvpanic;
 pub mod tpm;
 
 pub use self::acpi::{AcpiGedDevice, AcpiPmTimerDevice, AcpiShutdownDevice};
+pub use self::ivshmem::IvshmemDevice;
 pub use self::pvpanic::{PvPanicDevice, PVPANIC_DEVICE_MMIO_SIZE};
 
 bitflags! {
