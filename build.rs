6c08916fd936aa1d9c4e46e37b58592d7a67ec8a
Hai-Hsin <me@hai-hs.in>
2026-04-22 00:42:01 +0800

    feat(cli): add cube-hypervisor entrypoints and runtime wiring

--- build.rs
+++ build.rs
@@ -24,4 +24,6 @@ fn main() {
     // Particularly, this is used from src/main.rs to display the exact
     // version.
     println!("cargo:rustc-env=BUILT_VERSION={}", version);
+
+    println!("cargo:rustc-env=SNAPSHOT_VERSION=1.0.0");
 }
